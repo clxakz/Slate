8dca87f64e304cdfd79850a84937b73a43fac1c1
clxakz <zolotarjovdavid15@gmail.com>
2025-06-28 19:02:51 +0200

    update README

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer, contextBridge, app } from 'electron'
+import { ipcRenderer, contextBridge } from 'electron'
 
 contextBridge.exposeInMainWorld("api", {
     send: (channel: string, args: any) => ipcRenderer.invoke(channel, args),
